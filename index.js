301bc0260cb2aeb382227a737a3c872f2018854c
Jakestep <jakestep.17@gmail.com>
2023-03-05 16:29:13 -0700

    change computers

---
+++ index.js
@@ -0,0 +1,20 @@
+// const images = [url('images/img1.avif'), url('iamges/img2.png') , url('iamges/img3.jpg') , url('iamges/img4.gif')];
+// const renderEl = document.getElementById('image-shown');
+
+// function displayImage(src, width, height) {
+//     var img = document.createElement("img");
+//     img.src = src;
+//     img.width = width;
+//     img.height = height;
+//     renderEl.appendChild(img);
+// }
+
+// // displayImage(images[2], 40, 40)
+
+
+// document.getElementById("add-image").addEventListener('click', function(){
+//     // displayImage(images[1], 40, 40)
+//     console.log('clicked')
+// })
+
+console.log('clicked')
