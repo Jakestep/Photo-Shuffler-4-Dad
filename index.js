827b73b9115b3513477b6256e785f862ab5ed07d
Jakestep <jakestep.17@gmail.com>
2023-03-06 10:51:59 -0700

    hi

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const images = ['images/Body1.png','images/Body2.png','images/Body3.png','images/Body4.png','images/Body5.png','images/Body6.png','images/Body7.png','images/Body8.png','images/Body9.png','images/Body1.png','images/Body11.png','images/Body12.png','images/Body13.png','images/Body14.png','images/Body15.png','images/Body16.png','images/Body17.png','images/Body18.png','images/Body19.png'];
 const renderEl = document.getElementById('stick-pic');
-
+const wack = ['images/Body5.png', 'images/Body15.png']
 // document.getElementById('stick-pic').style.backgroundImage = `url(${images[0]})`
 
 function displayImage(src, width, height) {
@@ -13,14 +13,14 @@ function displayImage(src, width, height) {
 
 // displayImage(images[2], 40, 40)
 
-function shuffle() {
-    let whichPic = Math.floor(Math.random() * images.length)
+function shuffle(array) {
+    let whichPic = Math.floor(Math.random() * array.length)
     console.log(whichPic)
-    document.getElementById('stick-pic').style.backgroundImage = `url(${images[whichPic]})`
+    document.getElementById('stick-pic').style.backgroundImage = `url(${array[whichPic]})`
 }
 
 document.getElementById("shuffle-btn").addEventListener('click', function(){
-    shuffle()
+    shuffle(images)
 })
 window.refreshIntervalId;
 var shuffling = false;
@@ -30,7 +30,13 @@ document.getElementById('timer-btn').addEventListener('click', function() {
         shuffling = false
     }
     let time = document.getElementById('timer-input-el').value * 1000
-    window.refreshIntervalId = setInterval(function() {shuffle()}, time)
+    // if (time < 500) {
+    //     window.refreshIntervalId = setInterval(function() {shuffle(wack)}, time)
+    // } else {
+    //     window.refreshIntervalId = setInterval(function() {shuffle(images)}, time)
+    // }
+    window.refreshIntervalId = setInterval(function() {shuffle(images)}, time)
+    
     shuffling = true;
 })
 
