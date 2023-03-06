ca39cdf7a4544aca1513974a93eda9d846a92517
Jakestep <jakestep.17@gmail.com>
2023-03-06 09:41:01 -0700

    3/6/23

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const images = ['images/Body1.png','images/Body2.png','images/Body3.png','images/Body4.png','images/Body5.png','images/Body6.png','images/Body7.png','images/Body8.png','images/Body9.png','images/Body1.png0','images/Body1.png1','images/Body1.png2','images/Body1.png3','images/Body1.png4','images/Body1.png5','images/Body1.png6','images/Body1.png7','images/Body1.png8','images/Body1.png9','images/Body2.png0'];
+const images = ['images/Body1.png','images/Body2.png','images/Body3.png','images/Body4.png','images/Body5.png','images/Body6.png','images/Body7.png','images/Body8.png','images/Body9.png','images/Body1.png','images/Body11.png','images/Body12.png','images/Body13.png','images/Body14.png','images/Body15.png','images/Body16.png','images/Body17.png','images/Body18.png','images/Body19.png'];
 const renderEl = document.getElementById('stick-pic');
 
 // document.getElementById('stick-pic').style.backgroundImage = `url(${images[0]})`
@@ -13,11 +13,21 @@ function displayImage(src, width, height) {
 
 // displayImage(images[2], 40, 40)
 
-
-
-document.getElementById("shuffle-btn").addEventListener('click', function(){
+function shuffle() {
     let whichPic = Math.floor(Math.random() * images.length)
     console.log(whichPic)
     document.getElementById('stick-pic').style.backgroundImage = `url(${images[whichPic]})`
+}
+
+document.getElementById("shuffle-btn").addEventListener('click', function(){
+    shuffle()
 })
 
+document.getElementById('timer-btn').addEventListener('click', function() {
+    let time = document.getElementById('timer-input-el').value * 1000
+    window.refreshIntervalId = setInterval(function() {shuffle()}, time)
+})
+
+document.getElementById('stop-timer-btn').addEventListener('click', function() {
+    clearInterval(window.refreshIntervalId)
+})
